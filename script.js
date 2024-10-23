1607baf5409c7f43486dfc4015d3405e6f473705
Rodrigo Avelino Arruda da Silva <rodrigoarrudadasilva75@gmail.com>
2024-10-23 04:01:41 -0300

    Netflix
    Este projeto é uma aplicação front-end que simula a interface do serviço de streaming Netflix. O objetivo é recriar a experiência do usuário, apresentando uma navegação intuitiva e um design responsivo, utilizando as melhores práticas de desenvolvimento web.

---
+++ script.js
@@ -0,0 +1,33 @@
+
+let botaoSom = document.querySelector(".botao-som")
+let video = document.querySelector(".video")
+let botao = document.querySelector(".link-info")
+let modal = document.querySelector(".modal")
+let audio = document.querySelector(".audio")
+
+
+botaoSom.addEventListener("click", ligaSom)
+
+function ligaSom(){
+    video.muted =  !video.muted
+}
+
+botao.addEventListener("click", mostrarModal)
+modal.addEventListener("click", esconderModal)
+
+function mostrarModal(){
+    modal.style.display = "block"
+}
+
+function esconderModal(){
+    modal.style.display = "none"
+}
+
+// Tocar o TUNDUM
+
+window.addEventListener("load", tocarAudio)
+
+
+function tocarAudio(){
+    audio.play()
+}
